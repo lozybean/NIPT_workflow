6607f85ae5ae74d17b7c5a294978a538f13993a1
lyon <lozybean@163.com>
2018-02-01 19:12:15 +0800

    update plot function

--- script/bin_gc_correction.py
+++ script/bin_gc_correction.py
@@ -11,9 +11,12 @@ import re
 from collections import defaultdict
 from functools import lru_cache
 
-import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import pysam
+
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 re_variable = re.compile('variableStep chrom=(\w+) span=(\d+)')
 
@@ -46,9 +49,15 @@ class Work(object):
                             metavar='INT', type=int, default=30,
                             help="minimum mapping quality")
         parser.add_argument('-o', '--over_abundant', dest='over_abundant',
-                            metavar='INT', type=int, default=10,
+                            metavar='INT', type=int, default=5,
                             help="how many times of average depth will be recognized as "
-                                 "\"OVER ABUNDANT\", [default is 10]")
+                                 "\"OVER ABUNDANT\", [default is 5]")
+        parser.add_argument('--plot_bin_count', dest='plot_bin_count',
+                            metavar='FILE', type=str, default=None,
+                            help="plot bin count before and after gc correction")
+        parser.add_argument('--plot_distribution', dest='plot_distribution',
+                            metavar='FILE', type=str, default=None,
+                            help="plot reads distribution among bins")
         args = parser.parse_args()
         return args
 
@@ -121,65 +130,119 @@ class Work(object):
             count_bins[bin_key] += 1
         return count_bins
 
-    def average_depth_in_gc(self, gc_content):
+    def average_depth_in_gc(self, bin_counts, gc_content):
         bin_list = self.gc2bin[gc_content]
-        total_depth = sum(self.bin_counts[b] for b in bin_list)
+        total_depth = sum(bin_counts[b] for b in bin_list)
         return total_depth / len(bin_list)
 
     def gc_correct_bin_counts(self):
         rt_dict = defaultdict(int)
-        average_depth_dict = {}
+        average_depth_before_correct = {}
         weight_dict = {}
+        average_depth_after_correct = {}
         global_average_depth = self.get_global_average_depth()
         for gc_content, bin_list in self.gc2bin.items():
-            average_depth = self.average_depth_in_gc(gc_content)
-            average_depth_dict[gc_content] = average_depth
+            average_depth = self.average_depth_in_gc(self.bin_counts, gc_content)
+            average_depth_before_correct[gc_content] = average_depth
             weight = global_average_depth / average_depth
             weight_dict[gc_content] = weight
             for b in bin_list:
                 rt_dict[b] = self.bin_counts[b] * weight
-        return rt_dict, average_depth_dict, weight_dict
+            average_depth_after_correct[gc_content] = self.average_depth_in_gc(rt_dict, gc_content)
+        return rt_dict, average_depth_before_correct, average_depth_after_correct, weight_dict
 
     @staticmethod
-    def plot_gc_scatter(depth_dict, weight_dict):
-        depth_list = []
+    def plot_bin_count_scatter(depth_dict_before, depth_dict_after, weight_dict,
+                               out_file=None):
+        depth_list_before = []
+        depth_list_after = []
         weight_list = []
-        gc_list = sorted(depth_dict.keys())
+        gc_list = sorted(depth_dict_before.keys())
         for gc in gc_list:
-            depth_list.append(depth_dict[gc])
+            depth_list_before.append(depth_dict_before[gc])
+            depth_list_after.append(depth_dict_after[gc])
             weight_list.append(weight_dict[gc])
 
-        plt.subplot(1, 2, 1)
-        plt.scatter(gc_list, depth_list)
+        plt.figure(figsize=(10, 30))
 
-        plt.subplot(1, 2, 2)
+        plt.subplot(3, 1, 1)
+        plt.scatter(gc_list, depth_list_before)
+        plt.xlabel('gc content(%)')
+        plt.ylabel('average depth per bin')
+        plt.title("before correct")
+
+        plt.subplot(3, 1, 2)
+        plt.scatter(gc_list, depth_list_after)
+        plt.xlabel('gc content(%)')
+        plt.ylabel('average depth per bin')
+        plt.title("after correct")
+
+        plt.subplot(3, 1, 3)
         plt.scatter(gc_list, weight_list)
+        plt.xlabel('gc content(%)')
+        plt.ylabel('weight')
+        plt.title("weight")
 
-        plt.show()
+        if out_file:
+            plt.savefig(out_file, dpi=4000)
+        else:
+            plt.show()
         plt.close()
 
-    @staticmethod
-    def plot_distribution(raw_counts, cor_counts):
-        raw_depth = cor_depth = []
-        for b in raw_counts:
-            raw_depth.append(raw_counts[b])
-            cor_depth.append(cor_counts[b])
+    def plot_distribution(self, raw_counts, cor_counts, weight_dict, out_file=None):
+        def plot_chrom(chrom, ind):
+            dict_before = {p[1]: c for p, c in raw_counts.items() if p[0] == chrom}
+            dict_after = {p[1]: c for p, c in cor_counts.items() if p[0] == chrom}
+            plt.subplot(6, 4, ind)
+            index = sorted(dict_before.keys())
+            list_weight = []
+            list_before = []
+            list_after = []
+            for ind in index:
+                gc_content = self.bin2gc[(chrom, ind, ind + self.args.bin_size - 1)]
+                weight = weight_dict[gc_content]
+                list_weight.append(weight)
 
-        plt.subplot(1, 2, 1)
-        plt.plot(raw_depth)
+                list_before.append(dict_before[ind])
+                list_after.append(dict_after[ind])
+            epsilon = max(index)
+            index = np.array(index)
+            index = index * (1 / epsilon)
 
-        plt.subplot(1, 2, 2)
-        plt.plot(cor_depth)
+            plt.scatter(index, list_before, c='b', s=0.1, linewidth=0.35)
+            plt.scatter(index, list_after, c='r', s=0.1, linewidth=0.35)
+            plt.scatter(index, list_weight, c='y', s=0.1, linewidth=0.35)
+            plt.title(chrom, size=10)
+            plt.ylim(0, ymax)
+            plt.xlim(0, 1)
 
-        plt.show()
+        plt.figure(figsize=(10, 10))
+        ymax = max(max(raw_counts.values()), max(cor_counts.values()))
+
+        for i in range(1, 23):
+            chrom = f'chr{i}'
+            plot_chrom(chrom, i)
+        plot_chrom('chrX', 23)
+        plot_chrom('chrY', 24)
+
+        plt.subplots_adjust(hspace=0.5)
+
+        if out_file:
+            plt.savefig(out_file, dpi=4000)
+        else:
+            plt.show()
         plt.close()
 
     def __call__(self, *args, **kwargs):
-        gc_correct_depth, average_depth_dict, weight_dict = self.gc_correct_bin_counts()
+        gc_correct_depth, average_depth_before, average_depth_after, weight_dict = self.gc_correct_bin_counts()
 
-        self.plot_gc_scatter(average_depth_dict, weight_dict)
+        if self.args.plot_bin_count is not None:
+            self.plot_bin_count_scatter(average_depth_before, average_depth_after, weight_dict,
+                                        out_file=self.args.plot_bin_count)
 
-        self.plot_distribution(self.bin_counts, gc_correct_depth)
+        if self.args.plot_distribution is not None:
+            self.plot_distribution(self.bin_counts, gc_correct_depth, weight_dict,
+                                   out_file=self.args.plot_distribution)
 
 
 if __name__ == '__main__':
